a09fb0d31e44ff4c02534c40603bf59885971065
LennertJ <32568318+LennertJ@users.noreply.github.com>
2024-09-21 12:18:10 +0200

    oefening papegaai

--- startcode/a_papegaai.py
+++ startcode/a_papegaai.py
@@ -1,2 +1,9 @@
 # Schrijf een Python-programma dat de gebruiker vraagt om een tekst in te voeren en deze tekst vervolgens afdrukt.
 # Kies een gepaste naam voor je variabele.
+# from startcode.snippets import leeftijd
+
+naam = input("Wat is jouw naam?")
+print("hello " + naam)
+
+leeftijd = int(input("Wat is jouw leeftijd?"))
+print("volgend jaar wordt je ", leeftijd + 1)
